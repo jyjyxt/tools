e364e73bcab36924a2b4c505b7ec9834d2d01673
JyXt <im.jyjyxt@gmail.com>
2024-03-09 20:06:42 +1100

    Fix home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import site from "@/data/siteMetadata"
+import tools from "@/data/tools"
 
 export const metadata: Metadata = {
   title: `${site.title} | ${site.name}`,
@@ -9,10 +10,25 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
+  const list = tools.map((t) => {
+    return (
+      <a href={t.link} key={t.short} className="rounded-sm border border-stroke bg-white px-5 py-4 shadow-default dark:border-strokedark dark:bg-boxdark w-80">
+        <h2 className="text-lg mb-2 line-clamp-1">
+          {t.short}
+        </h2>
+        <div className="line-clamp-2">
+          {t.desc}
+        </div>
+      </a>
+    )
+  })
+
   return (
     <>
       <DefaultLayout>
-        <div>Tools</div>
+        <div className="flex flex-wrap">
+          {list}
+        </div>
       </DefaultLayout>
     </>
   );
