3070ecd5a979ed38f4f8ad40553057b98135dfbb
JyXt <im.jyjyxt@gmail.com>
2024-03-10 20:13:50 +1100

    Fix

--- src/app/uuid/client.tsx
+++ src/app/uuid/client.tsx
@@ -87,7 +87,7 @@ const Page = ({title} : {title: string | undefined}) => {
         </div>
         <div className="p-6.5">
           <div className="mb-6">
-            <div className="flex mb-5">
+            <div className="flex flex-wrap mb-5">
               <div className="flex items-center me-4">
                 <input checked={nl} id="inline-nil-radio" type="radio" value="" name="inline-radio-group" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={handleChange} />
                 <label htmlFor="inline-nil-radio" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">UUID NIL</label>
